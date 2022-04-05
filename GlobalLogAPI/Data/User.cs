87a44fc65662b5b8f38e6c502fe0778855c169e1
paritas-t <paritas.t@softthai.com>
2022-04-05 16:42:00 +0700

    Update

--- GlobalLogAPI/Data/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GlobalLogAPI.Data
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string? sFirst_Name { get; set; }
-        public string? sLast_Name { get; set; }
-    }
-}
