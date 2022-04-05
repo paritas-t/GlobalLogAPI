3f45132c99aa0c9ed7789a4ccdb9fa62243f3a49
paritas-t <paritas.t@softthai.com>
2022-04-05 09:25:51 +0700

    global cors policy

--- GlobalLogAPI/Program.cs
+++ GlobalLogAPI/Program.cs
@@ -3,9 +3,18 @@ using GlobalLogAPI.Extensions;
 using LoggerService;
 using NLog;
 using GlobalLogAPI.CustomExceptionMiddleware;
+using GlobalLogAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//var services = builder.Services;
+//var env = builder.Environment;
+// use sql server db in production and sqlite db in development
+//if (env.IsProduction())
+//    services.AddDbContext<DataContext>();
+//else
+//    services.AddDbContext<DataContext, SqliteDataContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,8 +29,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-//Log Error Global
-app.UseMiddleware<ExceptionMiddleware>();
+// global cors policy
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+////Log Error Global
+//app.UseMiddleware<ExceptionMiddleware>();
+// global error handler
+app.UseMiddleware<ErrorHandlerMiddleware>();
 
 app.UseHttpsRedirection();
 
